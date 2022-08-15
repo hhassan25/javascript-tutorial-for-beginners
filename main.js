0e3d23eb591f408da28c1a7ecff7b4be83299d7b
hadi <ha.hassan2510@hotmail.com>
2022-08-16 01:07:57 +0300

    session 5 all changes together

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ a = 'Hadi'
 a = true
 console.log(a)*/
 
-let x = 10
+/*let x = 10
 // = (equal) is the assignment operator
 
 let y = 5
@@ -85,4 +85,34 @@ console.log(!isValid)
 console.log('Hadi ' + 'Hassan')
 
 const isEven = 10 % 2 == 0 ? true : false
-console.log(isEven)
+console.log(isEven)*/
+
+
+//here getting started with implicit conversion
+console.log(2 + '3')
+console.log(true + '3')
+console.log('4' - '2')
+console.log('hadi' - 'hassan')
+console.log('5' - true)
+console.log('5' - false)
+console.log('5' - null)
+console.log(5 + undefined)
+
+
+
+console.log(Number('5'))
+console.log(Number(false))
+console.log(Number(''))
+console.log(parseInt('5'))
+console.log(parseFloat('5.44'))
+
+
+
+console.log(String(500))
+console.log(String(true))
+console.log(String(null))
+console.log(String(undefined))
+
+
+console.log((500).toString())
+console.log(Boolean(10))// null undefined '' Nan

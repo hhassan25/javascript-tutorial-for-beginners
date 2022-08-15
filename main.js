3d9f307a95b6b957a9be663a27309e9dd1e8fe65
hadi <ha.hassan2510@hotmail.com>
2022-08-16 00:10:11 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('hello from main.js')
+
+
+//variables
+let age = 25
+console.log(age)
+
+
+const salary = 80000
+console.log(salary)
+//const cannot be empty and cannot be changed
+//salary = 1
+
+let sum = 0
+sum = 6
+console.log(sum)

13e5cb9be34b5da1d32c46d8e5967b37092ebcbb
hadi <ha.hassan2510@hotmail.com>
2022-08-16 00:54:54 +0300

    last commit session 4

--- main.js
+++ main.js
@@ -53,3 +53,36 @@ a = 'Hadi'
 a = true
 console.log(a)*/
 
+let x = 10
+// = (equal) is the assignment operator
+
+let y = 5
+console.log(x + y)
+console.log(x % y)
+console.log(x++)
+// +, -, *, /, % are the arithmetic operators
+
+
+console.log(x == y)
+console.log(x != y)
+console.log(x > y)
+console.log(x < y)
+console.log(x >= y)
+console.log(x <= y)
+//these are comparison opertors that compare and return a boolean
+
+
+const isValidNumber = x>8 && 8>y
+const isValidNumberNew = x>8 || 8>y
+console.log(isValidNumber)
+console.log(isValidNumberNew)
+
+const isValid = false
+console.log(!isValid)
+//and, or are logical operators
+
+
+console.log('Hadi ' + 'Hassan')
+
+const isEven = 10 % 2 == 0 ? true : false
+console.log(isEven)
